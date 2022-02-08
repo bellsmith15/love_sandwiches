539ed467a523629ccdc4b5cb617d80d484694493
bellsmith15 <smith27n@gmail.com>
2022-02-08 22:11:08 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -23,6 +23,7 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 # print(data)
 
+
 def get_sales_data():
     """
     Get sales figures input from the user
@@ -45,6 +46,7 @@ def get_sales_data():
             break
 
     return sales_data
+
 
 def validate_data(values):
     """
@@ -90,10 +92,16 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    # print(stock_row)
     # pprint(stock)
     # print(get_all_values)
 
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    # print(surplus_data)
+    return surplus_data
 
 
 def main():
@@ -103,12 +111,12 @@ def main():
     data = get_sales_data()
     sales_data = [int(num)for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
 
 
 
